0eb8d0b6b8a1df72df49039d098e5776392efc71
jose clisanchez <xuqiaose@gmail.com>
2025-02-08 14:11:42 -0500

    0.6

--- almacen-app/src/index.js
+++ almacen-app/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Si deseas medir el rendimiento de tu aplicación, puedes pasar una función
+// para registrar los resultados (por ejemplo: reportWebVitals(console.log))
+// o enviar a un endpoint de análisis. Aprende más: https://bit.ly/CRA-vitals
 reportWebVitals();
